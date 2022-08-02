d425defb508fb3aaaab9e0ccd5844975f2424d8a
Elad Kaplan <kaplan.elad@gmail.com>
2022-08-03 00:22:36 +0300

    implement urlquery
    Signed-off-by: Elad Kaplan <kaplan.elad@gmail.com>

--- src/builtins/impls/urlquery.rs
+++ src/builtins/impls/urlquery.rs
@@ -16,28 +16,70 @@
 
 use std::collections::HashMap;
 
-use anyhow::{bail, Result};
+use serde_json::Value;
+use url::form_urlencoded::{byte_serialize, parse};
+
+const QUERY_CONCAT_CHAR: &str = "&";
 
 /// Decodes a URL-encoded input string.
-#[tracing::instrument(name = "urlquery.decode", err)]
-pub fn decode(x: String) -> Result<String> {
-    bail!("not implemented");
+#[tracing::instrument(name = "urlquery.decode")]
+pub fn decode(x: String) -> String {
+    parse(x.as_bytes())
+        .map(|(key, val)| [key, val].concat())
+        .collect()
 }
 
 /// Decodes the given URL query string into an object.
-#[tracing::instrument(name = "urlquery.decode_object", err)]
-pub fn decode_object(x: String) -> Result<HashMap<String, Vec<String>>> {
-    bail!("not implemented");
+#[tracing::instrument(name = "urlquery.decode_object")]
+pub fn decode_object(x: String) -> HashMap<String, Vec<String>> {
+    let mut decoded_object: HashMap<String, Vec<String>> = HashMap::new();
+    for pair in x.split(QUERY_CONCAT_CHAR) {
+        let mut iter_parameter = pair.split('=').take(2);
+        let (parameter_key, parameter_value) = match (iter_parameter.next(), iter_parameter.next())
+        {
+            (Some(k), Some(v)) => (k, v),
+            _ => continue,
+        };
+        match decoded_object.get_mut(parameter_key) {
+            Some(v) => v.push(parameter_value.to_string()),
+            _ => {
+                decoded_object.insert(parameter_key.to_string(), vec![parameter_value.to_string()]);
+            }
+        };
+    }
+    decoded_object
 }
 
 /// Encodes the input string into a URL-encoded string.
-#[tracing::instrument(name = "urlquery.encode", err)]
-pub fn encode(x: String) -> Result<String> {
-    bail!("not implemented");
+#[tracing::instrument(name = "urlquery.encode")]
+pub fn encode(x: String) -> String {
+    byte_serialize(x.as_bytes()).collect()
 }
 
 /// Encodes the given object into a URL encoded query string.
-#[tracing::instrument(name = "urlquery.encode_object", err)]
-pub fn encode_object(x: HashMap<String, serde_json::Value>) -> Result<String> {
-    bail!("not implemented");
+#[tracing::instrument(name = "urlquery.encode_object")]
+pub fn encode_object(x: HashMap<String, serde_json::Value>) -> String {
+    let mut encoded_object: Vec<String> = Vec::new();
+    x.iter()
+        .for_each(|(parameter_key, parameter_value)| match &parameter_value {
+            Value::Array(arr) => {
+                for v in arr.iter() {
+                    encoded_object
+                        .push(concat_encode_query(parameter_key, v.as_str().unwrap_or("")));
+                }
+            }
+            _ => {
+                encoded_object.push(concat_encode_query(
+                    parameter_key,
+                    parameter_value.as_str().unwrap_or(""),
+                ));
+            }
+        });
+    encoded_object.sort();
+    encoded_object.join(QUERY_CONCAT_CHAR)
+}
+
+fn concat_encode_query(key: &str, value: &str) -> String {
+    let encode_value: String = byte_serialize(value.as_bytes()).collect();
+    format!("{}={}", key, encode_value)
 }
---
+++ tests/infra-fixtures/test-urlquery.rego
@@ -0,0 +1,9 @@
+package test
+
+encode := urlquery.encode("?foo=1&bar=test")
+
+encode_object := urlquery.encode_object({"foo": "1", "bar": "foo&foo", "arr": ["foo", "bar"], "obj": {"obj1", "obj2"}})
+
+decode := urlquery.decode("%3Ffoo%3D1%26bar%3Dtest")
+
+decode_object := urlquery.decode_object("arr=foo&arr=bar&bar=test&foo=1&obj=obj1&obj=obj2")
