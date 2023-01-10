519ae160d5cb7982f1c03ff91465be52103da0fb
Elad Kaplan <kaplan.elad@gmail.com>
2023-01-10 18:11:33 +0200

    fix clippy

--- src/bin/simple.rs
+++ src/bin/simple.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![deny(clippy::pedantic)]
+
 use std::collections::HashMap;
 
 use anyhow::Result;
